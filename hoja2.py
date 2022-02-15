5466d80593d73878145477e9ed02bf405c19bd7d
Alejandro Placer <aplazer@ucm.es>
2022-02-15 17:13:53 +0100

    esta

--- hoja2.py
+++ hoja2.py
@@ -1,16 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Ejercicio 1
-'''arr = np.random.uniform(1, -1, 44100)
+def ejercicio1():
+    arr = np.random.uniform(1, -1, 44100)
 
-plt.plot(arr)
-plt.show()'''
+    plt.plot(arr)
+    plt.show()
 
-# Ejercicio 2
-arr = np.random.uniform(1, -1, 44100)
-t = 44100 - 1
-signal = np.sin(2*np.pi)*arr
+def ejercicio2():
+    arr = np.random.uniform(1, -1, 44100)
+    t = 44100 - 1
+    signal = np.sin(2*np.pi)*arr
 
-plt.plot(arr, signal)
-plt.show()
+    plt.plot(arr, signal)
+    plt.show()
+
+#ejercicio1()
+#ejercicio2()
+#ejercicio3()
+#ejercicio4()
+#ejercicio5()
+#ejercicio6()
